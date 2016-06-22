f2263a730787e9c968e8470b76240c89a7cd4d6b
aevista <aevistax@gmail.com>
2016-06-21 20:38:34 -0500

    added model and user authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     def base_title title = ''
         sub_title = 'Ruby on Rails Sample App'
         full_title = sub_title
-        full_title = " #{title} | #{sub_title}" if !title.empty?
+        full_title = "#{title} | #{sub_title}" if !title.empty?
         
         full_title
     end
